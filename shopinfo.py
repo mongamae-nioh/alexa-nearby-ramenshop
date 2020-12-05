3d0ddf042302df5ff66b950cbb35583fa137fe11
もんがまえ仁王 <jkawaoka@gmail.com>
2020-12-05 11:12:32 +0900

    レスポンスを整形するクラス（shopInfo）実装

--- shopinfo.py
+++ shopinfo.py
@@ -6,7 +6,7 @@ import os
 class restrantApi:
     def __init__(self, category1, category2):
         self.url = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
-        self.keyid = "apikey
+        self.keyid = "apikey"
         self.category1 = category1
         self.category2 = category2
         
@@ -95,30 +95,72 @@ class apiRequest:
     def total_page(self):
         res = self.api_request()
         total_page = math.ceil(res['total_hit_count'] / res['hit_per_page'])
-        
 
-param = restrantApi('RSFST08000', 'RSFST08008')
+    def current_page(self):
+        current_page = {}
+        current_page['offset_page'] = 1
+
+        return current_page
+    
+    def range(self):
+        range = {}
+        range['range'] = 1
+
+        return range        
+
+class shopInfo(apiRequest):
+    def __init__(self):
+        super().__init__(url, param)
+    
+    def shop_info(self):
+        shop_data = self.api_request()     
+        total_page = self.total_page()
+        page = 1
+
+        shopinfo = {}
+#        while total_page >= page:
+        while 4 >= page:
+            for shop in shop_data['rest']:
+                shopinfo[shop['name']] = shop['name_kana']
+            page += 1
+#            param = self.api_parameter()
+            param = self.param
+            param['offset_page'] = page
+            response = requests.get(self.url, params=param)
+            shop_data = response.json()
+
+        return shopinfo    
+
+
+param1 = restrantApi('RSFST08000', 'RSFST08008')
 #print(param.baseinfo())
 
-url = param.url
+url = param1.url
 param2 = geoLocation("43.0555316", "141.3526345")
 #print(param2.geolocation())
 
 
 param3 = mergeApiParameter()
-mergeparam = param3.api_parameter(param.baseinfo(), param2.geolocation())
-print(mergeparam)
+param = param3.api_parameter(param1.baseinfo(), param2.geolocation())
+#print(mergeparam)
 
-res1 = apiRequest(url, mergeparam)
+res1 = apiRequest(url, param)
 res2 = res1.api_request()
-
-print(res2)
+#print(res2)
 
 hit_count = res1.hit_count()
-print(hit_count)
+#print(hit_count)
 
 total_page = res1.total_page()
-print(total_page)
+#print(total_page)
+
+shop = shopInfo()
+
+print('url is ' + shop.url)
+print(shop.param)
+
+shopinfo = shop.shop_info()
+print(shopinfo)
 
 '''
 class apiRequest:
