0e0820b05dbe9918fa5742d5462db1fc1ccd2f15
もんがまえ仁王 <jkawaoka@gmail.com>
2020-12-05 16:43:41 +0900

    クラス名、関数名修正

--- shopinfo.py
+++ shopinfo.py
@@ -18,7 +18,7 @@ class restrantApi:
         
         return baseinfo
 
-class kuchikomiApi:
+class reputationApi:
     def __init__(self, menu):
         self.url = "https://api.gnavi.co.jp/PhotoSearchAPI/v3/"
         self.keyid = "apikey"
@@ -44,11 +44,11 @@ class geoLocation:
         return geolocation    
 
 class mergeApiParameter:
+    '''APIリクエストのパラメータとして使うために複数の辞書をマージするクラス'''
     def __init__(self):
         pass
 
     def api_parameter(self, *args):
-        '''APIリクエストのパラメータとして使うために複数の辞書をマージするクラス'''
         parameter = {}
 
         for i in args:
@@ -106,68 +106,52 @@ class restrantInfo(apiRequest):
     def __init__(self, url, param):
         super().__init__(url, param)
     
-    def shop_info(self):
+    def restrant_search(self):
         shop_data = self.api_request()     
         total_page = self.total_page()
         page = 1
 
-        shopinfo = {}
+        restrant_info = {}
         while total_page >= page:
             for shop in shop_data['rest']:
-                shopinfo[shop['name']] = shop['name_kana']
+                restrant_info[shop['name']] = shop['name_kana']
             page += 1
             param = self.param
             param['offset_page'] = page
             response = requests.get(self.url, params=param)
             shop_data = response.json()
 
-        return shopinfo    
+        return restrant_info    
 
-class kuchikomiInfo(apiRequest):
+class reputationInfo(apiRequest):
     def __init__(self, url, param):
         super().__init__(url, param)
     
-    def shop_info(self):
-        shop_data2 = self.api_request()
-        shop_data = shop_data2['response']
-#        print(shop_data)
+    def reputation_search(self):
+        shop_data = self.api_request()['response']
         per_page = shop_data['hit_per_page']
-        total_page = self.total_page2()
-        print('total page is ' + str(total_page))
         hit_count = self.hit_count2()
-        print('hit count is ' + str(hit_count))
         page = 1
 
-        kuchikomiinfo = {}
-        nokori = hit_count - (per_page * page)
+        reputation_info = {}
 
-        while hit_count - (per_page * page) > 0:
-            print('nokori is ' + str(nokori))
-            print('全部表示する')
+        while hit_count - (page * per_page) > 0:
             for i in range(per_page):
-                kuchikomiinfo[shop_data[str(i)]['photo']['shop_name']] = shop_data[str(i)]['photo']['comment']
-                print(i)
-                print(shop_data[str(i)]['photo']['shop_name'])
+                reputation_info[shop_data[str(i)]['photo']['shop_name']] = shop_data[str(i)]['photo']['comment']
             page += 1
             param = self.param
             param['offset_page'] = page
-            print(param)
-            print(url)
             response = requests.get(self.url, params=param)
-            shop_data3 = response.json()
-            shop_data = shop_data3['response']
+            shop_data = response.json()['response']
         else:
-            nokori2 = hit_count - (per_page * (page-1))
-            print('nokori2 is ' + str(nokori2))
-            print('残りだけ表示する')
-            for i in range(nokori2):
-                print(i)
-                print(shop_data[str(i)]['photo']['shop_name'])
+            remaining = hit_count - ((page-1) * per_page)
+            for i in range(remaining):
+                reputation_info[shop_data[str(i)]['photo']['shop_name']] = shop_data[str(i)]['photo']['comment']
             
-        return kuchikomiinfo    
+        return reputation_info    
 
 #param1 = restrantApi('RSFST08000', 'RSFST08008')
-param1 = kuchikomiApi('ラーメン')
+param1 = reputationApi('ラーメン')
 apibase = param1.baseinfo()
 
 param2 = geoLocation("43.0555316", "141.3526345")
@@ -179,9 +163,9 @@ param = merge.api_parameter(apibase, geolocation)
 url = param1.url
 
 #shop = restrantInfo(url, param)
-shop = kuchikomiInfo(url, param)
-shop2 = shop.shop_info()
-#print(shop2)
+shop = reputationInfo(url, param)
+shop2 = shop.reputation_search()
+print(shop2)
 
 #json = json.dumps(info, indent=4, ensure_ascii=False)
 #print(json)
