45ee427fb2aafb05634c7b36bd4069e2207af193
もんがまえ仁王 <jkawaoka@gmail.com>
2020-12-05 22:55:05 +0900

    変数名修正、未使用クラス削除

--- shopinfo.py
+++ shopinfo.py
@@ -104,13 +104,6 @@ class apiRequest:
 
         return range        
 
-class removeUnnecessaryCharactor:
-    def __init__(self, object):
-        self.object = object
-    
-    def remove_linefeed(self):
-        return self.object.replace('\r\n', '')
-
 class restrantInfo(apiRequest):
     def __init__(self, url, param):
         super().__init__(url, param)
@@ -142,22 +135,11 @@ class reputationInfo(apiRequest):
         hit_count = self.hit_count2()
         page = 1
 
-        reputation_info = {}
+        temp_reputation_info = {}
         index = 0
         while hit_count - (page * per_page) > 0:
             for i in range(per_page):
-#                reputation_info.update({
-#                    index: {
-#                        shop_data[str(i)]['photo']['shop_name']: { 
-#                            "menu": shop_data[str(i)]['photo']['menu_name'],
-#                            "comment": shop_data[str(i)]['photo']['comment'].replace('\r\n', ''),
-#                            "score": shop_data[str(i)]['photo']['total_score'],
-#                            "distance": shop_data[str(i)]['photo']['distance'],
-#                            "url": shop_data[str(i)]['photo']['shop_url']
-#                        }
-#                    }})
-#                index += 1
-                reputation_info.update({
+                temp_reputation_info.update({
                     shop_data[str(i)]['photo']['shop_name']: { 
                         "menu": shop_data[str(i)]['photo']['menu_name'],
                         "comment": shop_data[str(i)]['photo']['comment'].replace('\r\n', ''),
@@ -174,7 +156,7 @@ class reputationInfo(apiRequest):
         else:
             remaining = hit_count - ((page-1) * per_page)
             for i in range(remaining):
-                reputation_info.update({
+                temp_reputation_info.update({
                     shop_data[str(i)]['photo']['shop_name']: { 
                         "menu": shop_data[str(i)]['photo']['menu_name'],
                         "comment": shop_data[str(i)]['photo']['comment'].replace('\r\n', ''),
@@ -184,28 +166,28 @@ class reputationInfo(apiRequest):
                     }
                 })
         
-        last_dict = {}
+        reputation_info = {}
         index = 0
-        for i,j in reputation_info.items():
-            last_dict.update({
+        for i in temp_reputation_info.items():
+            reputation_info.update({
                 index: {
-                    i : {
-                        "menu": reputation_info[i]['menu'],
-                        "comment": reputation_info[i]['comment'],
-                        "distance": reputation_info[i]['distance'],
-                        "url": reputation_info[i]['url']
+                    "name": temp_reputation_info[i]['name'],
+                    "menu": temp_reputation_info[i]['menu'],
+                    "comment": temp_reputation_info[i]['comment'],
+                    "distance": temp_reputation_info[i]['distance'],
+                    "url": temp_reputation_info[i]['url']
                     }
                 }
-            })
+            )
             index += 1
         
-        last = json.dumps(last_dict, indent=4, ensure_ascii=False)
+        last = json.dumps(reputation_info, indent=4, ensure_ascii=False)
         print(last)
                 
         return reputation_info
 
-#param1 = restrantApi('RSFST08000', 'RSFST08008')
-param1 = reputationApi('ラーメン')
+param1 = restrantApi('RSFST08000', 'RSFST08008')
+#param1 = reputationApi('ラーメン')
 apibase = param1.baseinfo()
 
 param2 = geoLocation("43.0555316", "141.3526345")
@@ -216,12 +198,12 @@ param = merge.api_parameter(apibase, geolocation)
 
 url = param1.url
 
-#shop = restrantInfo(url, param)
-shop = reputationInfo(url, param)
-#shop2 = shop.restrant_search()
-shop2 = shop.reputation_search()
+shop = restrantInfo(url, param)
+#shop = reputationInfo(url, param)
+shop2 = shop.restrant_search()
+#shop2 = shop.reputation_search()
 #hitcount = shop.hit_count2()
-#print(shop2)
+print(shop2)
 
 #for i,j in shop2.items():
 #    mojiretsu = i + 'の口コミです。'
