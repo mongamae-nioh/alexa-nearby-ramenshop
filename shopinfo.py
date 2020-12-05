3d324da1627825abe9140c7f440a31e7255a922f
もんがまえ仁王 <jkawaoka@gmail.com>
2020-12-05 15:08:13 +0900

    口コミAPIのレスポンス形式に合わせたクラスと関数の実装

--- shopinfo.py
+++ shopinfo.py
@@ -48,7 +48,7 @@ class mergeApiParameter:
         pass
 
     def api_parameter(self, *args):
-        '''複数の辞書をマージするクラス'''
+        '''APIリクエストのパラメータとして使うために複数の辞書をマージするクラス'''
         parameter = {}
 
         for i in args:
@@ -78,6 +78,18 @@ class apiRequest:
         
         return total_page
 
+    def hit_count2(self):
+        res = self.api_request()
+        hitcount = res['response']['total_hit_count']
+
+        return hitcount
+        
+    def total_page2(self):
+        res = self.api_request()
+        total_page = math.ceil(res['response']['total_hit_count'] / res['response']['hit_per_page'])
+        
+        return total_page
+
     def current_page(self):
         current_page = {}
         current_page['offset_page'] = 1
@@ -90,7 +102,7 @@ class apiRequest:
 
         return range        
 
-class shopInfo(apiRequest):
+class restrantInfo(apiRequest):
     def __init__(self, url, param):
         super().__init__(url, param)
     
@@ -111,19 +123,63 @@ class shopInfo(apiRequest):
 
         return shopinfo    
 
-param1 = restrantApi('RSFST08000', 'RSFST08008')
+class kuchikomiInfo(apiRequest):
+    def __init__(self, url, param):
+        super().__init__(url, param)
+    
+    def shop_info(self):
+        shop_data2 = self.api_request()
+        shop_data = shop_data2['response']
+        per_page = shop_data['hit_per_page']
+        total_page = self.total_page2()
+        page = 1
+
+        kuchikomiinfo = {}
+
+        for i in range(per_page):
+            kuchikomiinfo[shop_data[str(i)]['photo']['shop_name']] = shop_data[str(i)]['photo']['comment']
+#            print(shop_data[str(i)]['photo']['shop_name'])
+#            kuchikomiinfo['shop_name'] = shop_data[str(i)]['photo']['shop_name']
+        
+        print(kuchikomiinfo)
+        return kuchikomiinfo
+
+'''
+        while total_page >= page:
+            for i in range(0, per_page):
+                print(shop_data[str(i)]['photo']['shop_name'])
+                kuchikomiinfo['shop_name'] = shop_data[str(i)]['photo']['shop_name']
+
+            page += 1
+            param = self.param
+            param['offset_page'] = page
+            response = requests.get(self.url, params=param)
+            shop_data2 = response.json()
+#            print(shop_data2)
+        
+        print(kuchikomiinfo)
+#            shop_data = shop_data2['response']
+
+        return kuchikomiinfo    
+'''
+
+#param1 = restrantApi('RSFST08000', 'RSFST08008')
+param1 = kuchikomiApi('ラーメン')
 apibase = param1.baseinfo()
 
 param2 = geoLocation("43.0555316", "141.3526345")
 geolocation = param2.geolocation()
 
 merge = mergeApiParameter()
-
-url = param1.url
 param = merge.api_parameter(apibase, geolocation)
 
-shop = shopInfo(url, param)
-shopinfo = shop.shop_info()
-hit_count = shop.hit_count()
-print(hit_count)
-print(shopinfo)
+url = param1.url
+
+#shop = restrantInfo(url, param)
+shop = kuchikomiInfo(url, param)
+shop2 = shop.shop_info()
+#print(shop2)
+
+#json = json.dumps(info, indent=4, ensure_ascii=False)
+#print(json)
+
